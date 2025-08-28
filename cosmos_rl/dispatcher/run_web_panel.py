874ca24ffbf0ec9f39bc520c59f11909072f1842
jiaxinc <jiaxinc@nvidia.com>
2025-08-28 21:51:52 +0800

    fix: min_filter_prefix_tokens corner case (#251)
    May fix this
    issue:https://github.com/nvidia-cosmos/cosmos-reason1/issues/54

--- cosmos_rl/dispatcher/run_web_panel.py
+++ cosmos_rl/dispatcher/run_web_panel.py
@@ -525,10 +525,14 @@ async def put_rollout_group(rollout: RolloutRequest):
                     rewards = [rollouts_group[i].reward for i in rollout_indices]
                     if len(set(rewards)) > 1:
                         n_ignore_prefix_tokens = len(shared_prefix)
+                        prefix_str = controller.tokenizer.decode(shared_prefix)
                         for rollout_index in rollout_indices:
-                            rollouts_group[
-                                rollout_index
-                            ].n_ignore_prefix_tokens = n_ignore_prefix_tokens
+                            # Only do this if shared_prefix != rollout.completion
+                            # Else the whole sample will be ignored, which cause training issues.
+                            if prefix_str != rollouts_group[rollout_index].completion:
+                                rollouts_group[
+                                    rollout_index
+                                ].n_ignore_prefix_tokens = n_ignore_prefix_tokens
                 valid_rollouts_list.append(rollouts_group)
             else:
                 # If the rewards are all the same, we need to sample one rollout from the group
